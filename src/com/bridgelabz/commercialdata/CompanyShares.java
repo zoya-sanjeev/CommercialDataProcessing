c9dba895235f065652ce58af3a38c65fa47b533f
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-09-05 22:24:26 +0530

    ADDED: CompanyShares class created

---
+++ src/com/bridgelabz/commercialdata/CompanyShares.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.commercialdata;
+
+import java.time.LocalDateTime;
+
+public class CompanyShares {
+	
+	private String symbol;
+	private int noOfShares;
+	private LocalDateTime datetime;
+	
+
+}
