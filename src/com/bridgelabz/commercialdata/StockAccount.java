81ee61ad60a5213e09682eb603af67739264971b
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-09-05 22:30:14 +0530

    ADDED: valueOf method added

--- src/com/bridgelabz/commercialdata/StockAccount.java
+++ src/com/bridgelabz/commercialdata/StockAccount.java
@@ -1,11 +1,14 @@
 package com.bridgelabz.commercialdata;
 
+import java.util.*;
+
 public class StockAccount {
 	private String fileName;
 	List<CompanyShares> companyShares;
 	
 	public StockAccount(String fileName) {
 		this.fileName=fileName;
+		companyShares=new ArrayList<>();
 	}
 	
 
