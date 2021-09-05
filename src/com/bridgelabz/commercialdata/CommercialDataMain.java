5d2ba21254666463e748022233945babaa24ac6a
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-09-05 23:48:50 +0530

    ADDED: CommercialDataMain created for main method

---
+++ src/com/bridgelabz/commercialdata/CommercialDataMain.java
@@ -0,0 +1,18 @@
+package com.bridgelabz.commercialdata;
+
+import java.time.LocalDateTime;
+
+public class CommercialDataMain {
+
+	public static void main(String[] args) {
+		StockAccount account=new StockAccount("NewAccount");
+		CompanyShares reliance=new CompanyShares("reliance", 20, LocalDateTime.now());
+		account.addCompanyShares(reliance);
+		CompanyShares tata=new CompanyShares("tata", 50, LocalDateTime.now());
+		account.addCompanyShares(tata);
+		account.buy(10,"reliance");
+		account.sell(10, "tata");
+		
+	}
+
+}
