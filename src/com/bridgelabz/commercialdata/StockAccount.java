9e94a6de7db6d3ac989780eeafc337c3888de739
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-09-05 21:50:33 +0530

    ADDED: class variables and constructor added

--- src/com/bridgelabz/commercialdata/StockAccount.java
+++ src/com/bridgelabz/commercialdata/StockAccount.java
@@ -1,5 +1,12 @@
 package com.bridgelabz.commercialdata;
 
 public class StockAccount {
+	private String fileName;
+	List<CompanyShares> companyShares;
+	
+	public StockAccount(String fileName) {
+		this.fileName=fileName;
+	}
+	
 
 }
